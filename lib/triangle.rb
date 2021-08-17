df43c0bf9301f291bc63a4de0228b1670458b71a
Trace <tracedelange@me.com>
2021-08-17 11:40:35 -0500

    commit tree dec59eab96c4dc92f5b5f562b65cbee39c4ef5c2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_reader :first, :second, :third, :kind
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+
+
+    if @first <= 0 || @second <= 0 || @third <= 0
+      raise TriangleError
+    elsif (@first + @second) <= @third || (@second + @third) <= @first || (@first + @third) <= @second 
+      raise TriangleError
+    else
+      if @first == @second && @second == @third
+        @kind = :equilateral
+      elsif @first == @second || @first == @third || @second == @third
+        @kind = :isosceles
+      elsif @first != @second && @first != @third && @second != @third
+        @kind = :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Bad shape! no cookie."
+    end
+  end
+
+  def kind
+    @kind
+  end
+
 end
